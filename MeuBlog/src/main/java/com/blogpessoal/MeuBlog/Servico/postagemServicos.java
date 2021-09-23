99b0c38921c23ec3d276e1fd6917f489291b11fc
AlysonRodrigo <alysonrodrigo14@hotmail.com>
2021-09-23 19:31:22 -0300

    criando classe servico postagem

---
+++ MeuBlog/src/main/java/com/blogpessoal/MeuBlog/Servico/postagemServicos.java
@@ -0,0 +1,42 @@
+package com.blogpessoal.MeuBlog.Servico;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.blogpessoal.MeuBlog.Model.postagemModel;
+import com.blogpessoal.MeuBlog.Model.temaModel;
+import com.blogpessoal.MeuBlog.Repository.postagemRepository;
+import com.blogpessoal.MeuBlog.Repository.temaRepository;
+import com.blogpessoal.MeuBlog.Repository.usuarioRepository;
+
+@Service
+public class postagemServicos {
+	private @Autowired postagemRepository repositorioP;
+	private @Autowired usuarioRepository repositorioU;
+	private @Autowired temaRepository repositorioT;
+	
+	public Optional<?> cadastrarPostagem(postagemModel novaPostagem) {
+		Optional<temaModel> objetoExistente = repositorioT.findById(novaPostagem.getTemasRelacionados().getIdTema());
+		return repositorioU.findById(novaPostagem.getCriador().getIdUsuario()).map(usuarioExistente -> {
+			if (objetoExistente.isPresent()) {
+				novaPostagem.setCriador(usuarioExistente);
+				novaPostagem.setTemasRelacionados(objetoExistente.get());
+				return Optional.ofNullable(repositorioP.save(novaPostagem));
+			} else {
+				return Optional.empty();
+			}
+		}).orElseGet(() -> {
+			return Optional.empty();
+		});
+	}
+	public Optional<postagemModel> alterarPostagem(postagemModel postagemParaAlterar) {
+		return repositorioP.findById(postagemParaAlterar.getIdPostagem()).map(postagemExistente -> {
+			postagemExistente.setTitulo(postagemParaAlterar.getTitulo());
+			postagemExistente.setDescricao(postagemParaAlterar.getDescricao());
+			return Optional.ofNullable(repositorioP.save(postagemExistente));
+		}).orElseGet(() -> {
+			return Optional.empty();
+		});
+	}
+}
